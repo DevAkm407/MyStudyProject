ecb220d70909cdeacc2c5815efa7536ed6e6aa0a
cvisonakm <cvisonakm@gamil.com>
2024-04-01 11:08:33 +0900

    그거

--- workspace/list.c
+++ workspace/list.c
@@ -2,30 +2,43 @@
 #include<stdlib.h>
 typedef struct list list;
 void listopen(list* w );
+int* memoryalloc(int* a);
 list* listappend(list* w,int b);
 list* listsort(list* w);
 void listfree(list* w);
 
+// 크게 할당함 그리고 할당한 주소의 맨끝에 \0 이런거넣어둠 그다음에 포인터가 주소를읽으면 사용
+// \0 주소의 끝이라고 알려주게함 그다음에 더큰 주소가 필요함 그러면 더크게 할당 malloc(이전꺼 보다 크게)
+// 이전데이터를 옮겨 줘야함 근본적인 방법 for문으로 바꿔주기 간편한방법 stdlib에 있는 메모리 copy함수 사용
+//이전데이터 주소는 더이상 필요없음 free(이전 주소)
+// 그냥 realloc 쓰기 다만 운없으면 실패
+// 구조체를 변수를 메인에 선언하는 이유 함수는 자기할일 끝나면 없어짐 그래서 함수가 자기일끝나면 메모리 해재함 동적제외
+//static을 함수에 넣으면 똑같이 메인이 끝날때 없어짐
 struct data
 {
-    list* address;
-    list* origin;
-    int a;
+    list* address; 
+    list* origin;  
+    int a; 
     
-    }data;
+    }data; 
 
 struct list
 {
-int count;
-struct data data;
+int count;         
+struct data data; 
 };
+typedef struct itme
+{
+   int a;
+}itme;
+
 
 int main()
 {
     list a;
     list* w=&a;
     w->data.a=1;
-    listopen(w);
+    listopen(w); 
     while(1){
         char a;
         printf("숫자를 넣을까요? Y OR N  ");
@@ -42,7 +55,7 @@ int main()
         while(getchar()!='\n')
             continue;        
     }
-    printf("카운트:%d\n",w->count);
+    
     for (int i = 0; i <= w->count; i++)
     {
         w= i==0 ? w->data.origin: w->data.address;
@@ -52,7 +65,6 @@ int main()
 
     return 0;
 }
-
 void listopen(list* w)
 {
     w->count=1;
@@ -81,5 +93,7 @@ void listfree(list* w)
         list* b=a->data.address;
         a=b->data.address;
         free(b);
+        
     }    
 }
+
---
+++ workspace/monster.c
@@ -0,0 +1,297 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+//맵구현+몬스터 배치  어려움(동적할당 사용함)(광민)
+typedef struct boki
+{
+    int fhp;
+    int chp;
+    int damage;
+}boki;
+
+struct oaks_warrior
+{
+    int damage;
+    int hp;
+    int gold;
+};
+
+struct zombie
+{
+    int damage;
+    int hp;
+    int gold;
+};
+
+struct ghoul
+{
+    int damage;
+    int hp;
+    int gold;
+};
+
+struct skeleton
+{
+    int damage;
+    int hp;
+    int gold;
+};
+
+struct spartoi
+{
+    int damage;
+    int hp;
+    int gold;
+};
+struct student
+{
+    int name;
+    int damage;
+    int hp;
+    int gold;
+};
+
+
+typedef struct monster
+{
+   struct oaks_warrior oaks_warrior;
+   struct zombie zombie;
+   struct ghoul ghoul;
+   struct skeleton skeleton;
+   struct spartoi spartoi;
+   struct student hero;
+}monster;
+struct npc
+{
+    /* data */
+};
+struct town
+{
+struct npc npc;
+int townmaps[50][50];
+
+};
+
+struct firstfloor
+{
+int firstfloormaps[50][50];
+int monstermaps[50][50];
+monster* first;
+};
+
+struct secondfloor
+{
+int secondfloormaps[50][50];
+int monstermaps[50][50];
+monster* second;
+};
+
+struct thirdfloor
+{
+int thirdfloormaps[50][50];
+int monstermaps[50][50];
+monster* third;
+};
+
+struct fourthfloor
+{
+int fourthfloormaps[50][50];
+int monstermaps[50][50];
+monster* fourth;
+};
+
+struct fifthfloor
+{
+int fifthfloormaps[50][50];
+int monstermaps[50][50];
+monster* fifth;
+};
+
+
+
+typedef struct maps
+{
+
+struct town town;
+struct firstfloor firstfloor;
+struct secondfloor secondfloor;
+struct thirdfloor thirdfloor;
+struct fourthfloor fourthfloor;
+struct fifthfloor fifthfloor;
+}maps;
+
+
+
+//function
+maps* MapsInitialization(maps*);
+maps* MapsReallocation(maps*);
+monster* MonsterInitialization(monster*);
+monster* MonterRegenerative(monster*,boki* user,int);
+maps* MonsterMapsRegenerative(maps*,int);
+int main (void)
+{
+
+}
+maps* MapsInitialization(maps* mapspointer)
+{
+    for (int i = 0; i < 50; i++)
+    {
+        for (int j = 0; j < 50; j++)
+        {
+            mapspointer->town.townmaps[i][j];
+            mapspointer->firstfloor.firstfloormaps[i][j];
+            mapspointer->secondfloor.secondfloormaps[i][j];
+            mapspointer->thirdfloor.thirdfloormaps[i][j];
+            mapspointer->fourthfloor.fourthfloormaps[i][j];
+            mapspointer->fifthfloor.fifthfloormaps[i][j];
+        }
+    }
+ return mapspointer;   
+}
+
+
+monster* MonsterInitialization(monster* imonster)
+{
+    srand(time(NULL));
+    
+    imonster->oaks_warrior.hp = (rand() % 51 )+50;
+    imonster->oaks_warrior.damage = (rand() % 6) +10;
+    
+    imonster->zombie.hp= (rand() % 131) +50;
+    imonster->zombie.damage =(rand() % 14) +17;
+    
+    imonster->ghoul.hp=(rand() % 61) +120;
+    imonster->ghoul.damage = (rand() % 26)+20;
+    
+    imonster->skeleton.hp = (rand() % 61)+200;
+    imonster -> skeleton.damage =(rand() % 28)+28;
+    
+    imonster->spartoi.hp =(rand()% 101)+260;
+    imonster->spartoi.damage = (rand()%44)+32;   
+}
+
+monster* MonterRegenerative(monster* rm,boki* user,int floor)
+{
+    srand(time(NULL));
+    switch (floor)
+    {
+    case 1:
+        rm->oaks_warrior.hp = (rand() % 51 )+50;
+        rm->oaks_warrior.damage = (rand() % 6) +10;
+        rm->hero.hp= user->chp * 2;
+        rm->hero.damage=(rand()%201)+100;
+        break;
+    case 2:
+        rm->zombie.hp= (rand() % 131) +50;
+        rm->zombie.damage =(rand() % 14) +17;
+        rm->hero.hp= user->chp * 2;
+        rm->hero.damage=(rand()%201)+100;
+        break;
+    case 3:
+        rm->ghoul.hp=(rand() % 61) +120;
+        rm->ghoul.damage = (rand() % 26)+20;
+        rm->hero.hp= user->chp * 2;
+        rm->hero.damage=(rand()%201)+100;
+        break;
+    case 4:
+        rm->skeleton.hp = (rand() % 61)+200;
+        rm -> skeleton.damage =(rand() % 28)+28;
+        rm->hero.hp= user->chp * 2;
+        rm->hero.damage=(rand()%201)+100;
+        break;
+    case 5:
+        rm->spartoi.hp =(rand()% 101)+260;
+        rm->spartoi.damage = (rand()%44)+32;   
+        rm->hero.hp= user->chp * 2;
+        rm->hero.damage=(rand()%201)+100;
+        break;
+    default:
+        break;
+    }
+}
+//매개변수로 maps*를 넣어야함 int floor는 몇층인지 나타냄 학생은 2 그 맵의 기본몬스터는 1 몬스터가 없으면 0
+maps* MonsterMapsRegenerative(maps* monmap,int floor)
+{
+   srand(time(NULL));
+   switch (floor)
+   {
+   case 1: 
+        for (int i = 0; i < 50; i++)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                int mmr=rand()%100;
+                if(mmr>=70)
+                    monmap->firstfloor.monstermaps[i][j]=2;
+                else if(mmr<=50)
+                    monmap->firstfloor.monstermaps[i][j]=1;
+                else
+                    monmap->firstfloor.monstermaps[i][j]=0;
+            }
+            
+        }
+        
+        break;
+    
+   case 2: 
+        for (int i = 0; i < 50; i++)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                int mmr=rand()%100;
+                if(mmr>=70)
+                    monmap->secondfloor.monstermaps[i][j]=2;
+                else if(mmr<=50)
+                    monmap->secondfloor.monstermaps[i][j]=1;
+                else
+                    monmap->secondfloor.monstermaps[i][j]=0;
+            }
+            
+            
+        }
+        break;
+        
+    case 3: 
+        for (int i = 0; i < 50; i++)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                int mmr=rand()%100;
+                if(mmr>=70)
+                    monmap->thirdfloor.monstermaps[i][j]=2;
+                else if(mmr<=50)
+                    monmap->thirdfloor.monstermaps[i][j]=1;
+                else
+                    monmap->thirdfloor.monstermaps[i][j]=0;
+            }
+            
+            
+        }
+        break;
+        
+    case 4: 
+        for (int i = 0; i < 50; i++)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                int mmr=rand()%100;
+                if(mmr>=70)
+                    monmap->fourthfloor.monstermaps[i][j]=2;
+                else if(mmr<=50)
+                    monmap->fourthfloor.monstermaps[i][j]=1;
+                else
+                    monmap->fourthfloor.monstermaps[i][j]=0;
+            }
+        }
+        break;
+    case 5: 
+    
+    break;
+   default:
+        fputs("floor 값이 이상합니다!!!",stdin);
+        break;
+   }
+   
+    return monmap;
+}
