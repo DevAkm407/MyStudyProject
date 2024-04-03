84def06f0951ecb136b9556516eb672ed47891bb
cvisonakm <cvisonakm@gamil.com>
2024-04-03 21:32:54 +0900

    오늘한거

---
+++ workspace/sh.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h> /*랜덤함수 사용 위해 불러옴*/
+#include<time.h> /*랜덤함수 사용 위해 불러옴*/
+#include"item.h"
+
+int town_movement(Boki* boki);/*이동아이템 획득 함수 전방선언*/
+int drop_2teer20(Boki* boki);/*2티어 20프로*/
+int drop_3teer20(Boki* boki);/*3티어 20프로*/
+int drop_3teer30(Boki* boki);/*3티어 30프로*/
+int drop_4teer5(Boki* boki);/*4티어 5프로*/
+int drop_4teer10(Boki* boki);/*4티어 10프로*/
+int drop_4teer20(Boki* boki);/*4티어 20프로*/
+
+int gold_drop(Boki* boki,int floor);/*골드 드랍 함수*/
+int gold_drop_boss(Boki* boki,int boss);/*보스 골드 드랍 함수*/
+
+int main(void)
+{
+
+}
+
+
+
+
+
