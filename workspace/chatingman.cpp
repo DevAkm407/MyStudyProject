ba3b22db7dd4b7964be3ffa98e883402487249b9
cvisonakm <cvisonakm@gamil.com>
2024-05-22 21:43:36 +0900

    dsadsa

--- workspace/chatingman.cpp
+++ workspace/chatingman.cpp
@@ -8,20 +8,31 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <pthread.h>
+#include <sys/epoll.h>
 #include"chatserver.hpp"
 #include<unordered_map>
-#define BUF_SZIE 2048
+#define BUF_SZIE 1024
 #define MAX_CLNT 256
+#define EPOLL_SIZE 50
 using namespace std;
+struct chatingparts
+{
+    int socks[30];
+    
+    int len;
+};
 void * handle_clnt(void* arg);
-int* oneread_oneread(string msg);
+chatingparts oneread_oneread(string msg);
 void chat_msg(string clnts,char * msg, int len);
+void gosock(chatingparts my);
 void send_msg(char * msg, int len);
 void error_handling(string message);
 void send_clnt(string msg,int sock);
+void* chatingroom(void* socks);
 string findonlinefriend(string msg);
-void chatprotocol(int checkf,int clnt_sock,int* login,chatserver* my,char msg[BUF_SZIE],string* aka);
-int plzone_one(int* login,int socks[2]);
+string splitchatmsg(string msg);
+void chatprotocol(int checkf,int clnt_sock,int* login,chatserver* my,char msg[BUF_SZIE],string* aka,int* status);
+int plzone_one(int* login,chatingparts my);
 int clnt_cnt=0;
 int clnt_socks[MAX_CLNT];
 unordered_map<int,int> clnt_fid;
@@ -69,19 +80,25 @@ int main(int argc,char *argv[])
 }
 
 
+
 void* handle_clnt(void* arg)
 {
     int clnt_sock=*((int*)arg);
-    int str_len=0,i;
+    int i;
+    int* status=new int;
+    int * str_len=new int;
     int* login=new int;
     char* msg=new char[BUF_SZIE];
     chatserver* my =new chatserver;
     string* aka=new string;
-    while ((str_len=read(clnt_sock,msg,BUF_SZIE))!=0)
+    while ((*str_len=read(clnt_sock,msg,BUF_SZIE))!=0)
     {              
-            msg[str_len]=0;
+    
+        if(*status==1)
+                continue;
+            msg[*str_len]=0;
             // puts(msg);
-            chatprotocol(atoi(&msg[0]),clnt_sock,login,my,msg,aka);
+            chatprotocol(atoi(&msg[0]),clnt_sock,login,my,msg,aka,status);
     }
     pthread_mutex_lock(&mutx);//만약 다른 클라이언트가 비슷한 시기에 접속을 종료했다면 밑에 for문이 돌기전에 진입하게된다.그렇게되면 소켓번호를
     for ( i = 0; i <clnt_cnt ; i++)//재정렬하는 과정에서 하나가 또 지워지거나 정렬하고 있는거 또 정렬해서 순번이 꼬이는 사건이 발생할 수 있다. 그 사건을 막기위한 lock
@@ -96,89 +113,121 @@ void* handle_clnt(void* arg)
     clnt_cnt--;
     pthread_mutex_unlock(&mutx);//모든 정렬이 끝나고나서 unlock
     clnt_fid.erase(*login);
+    delete str_len;
     delete login;
     delete msg; 
     close(clnt_sock);
     return NULL;
 }
 
-//사용자가 1:1채팅을 요청했을때 socket을 묶어주는 함수 sock을 반드시 delete할것 실패시 NULL 반환 성공시 클라이언트 소켓배열 반환 크기는 int*2 
-int* oneread_oneread(string msg)
+//사용자가 채팅을 요청했을때 socket을 묶어주는 함수 sock을 반드시 delete할것 실패시 NULL 반환 성공시 클라이언트 소켓배열 반환 크기는 int*2 
+chatingparts oneread_oneread(string msg)
 {
-    istringstream iss(msg);
+    string mssg;
+    mssg=splitchatmsg(msg);
+    stringstream ssg(mssg);
     string buffer;
-    vector<int> frinedsock;
+    chatingparts my;
     unordered_map<int,int>::iterator ater;
     int* sock=new int[2];
     memset(sock,0,sizeof(int)*2);
-    int count=0;
-    while (getline(iss,buffer,','))
-    {
-        frinedsock.emplace_back(stoi(buffer));
-    }
-    if(frinedsock.size()>2)
-        return NULL;
-
-    vector<int>::iterator iter;
-        
-    for (iter=frinedsock.begin(); iter!=frinedsock.end(); ++iter)
-    {
-        
-        ater=clnt_fid.find(*iter);
-        if(ater!= clnt_fid.end())
-        {
-           sock[count]=ater->second;    
-        }
-        count++;
-    }
-    return sock;
+    ssg>>sock[0]>>sock[1];
+    
+    ater=clnt_fid.find(sock[0]);
+    sock[0]=ater->second;
+    ater=clnt_fid.find(sock[1]);
+    sock[1]=ater->second;
+    my.socks[0]=sock[0];
+    my.socks[1]=sock[1];
+    my.len=2;
+    return my;
 }
 
 
 
 //요청한 상대에게 대화 요청 보내기 수락하면 1 아니면 0
-int plzone_one(int* login,int socks[2])
+int plzone_one(int* login,chatingparts my)
 {   
     ostringstream oss;
     string alk;
-    oss<<"2, "<<login<<"이 "<<"대화를 요청했습니다. 수락하려면 y 거절하려면 n";
+    int socks[2];
+    socks[0]=my.socks[0];
+    socks[1]=my.socks[1];
+    int ren;
+    oss<<"2, "<<*login<<"이 "<<"대화를 요청했습니다. 수락하려면 y 거절하려면 n";
     alk=oss.str();
     int* c = new int;
     char booksmsg[BUF_SZIE];
     alk.copy(booksmsg,alk.length());
     booksmsg[alk.length()+1]=0;
+
     write(socks[1],booksmsg,alk.length()+1);
     memset(booksmsg,0,BUF_SZIE);
-    read(socks[1],booksmsg,BUF_SZIE);
-    if(strcmp(booksmsg,"y")==0)
-        return 1;    
+    ren=read(socks[1],booksmsg,BUF_SZIE);
+    booksmsg[ren]=0;
+    alk=booksmsg;
+    if(alk.compare("y"))
+        return 1;
     delete c;   
     return 0;
 }
 
-
-//1:1채팅방 함수 msg를 통해 데이터가 들어오고 2명에게 모두 읽어온 데이터를 보냄
-void one_one(string msg,int socks[2])
-{   
-    istringstream iss(msg);
-    string mmsg;
-    int* c = new int;
-    char booksmsg[BUF_SZIE];
-    memset(booksmsg,0,BUF_SZIE);
-    pthread_mutex_lock(&mutx);
-    while (getline(iss,mmsg))
+void* okchatingroom(void* socks)
+{
+    int mysock=*(int*)socks;
+    chatingparts* real=new chatingparts;
+    int str_ren,i;
+    string k;
+    
+    char* buffer = new char[BUF_SZIE];
+    read(mysock,(void*)real,sizeof(chatingparts));    
+    
+       
+    while ((str_ren=read(mysock,buffer,BUF_SZIE))!= -1)
     {
-        mmsg=mmsg+"\n";
-        mmsg.copy(booksmsg,mmsg.length());
-        write(socks[0],booksmsg,mmsg.length());
-        write(socks[1],booksmsg,mmsg.length());
-        memset(booksmsg,0,BUF_SZIE);
-    }  
-    pthread_mutex_unlock(&mutx);
-    delete c;   
-    delete socks;
+        buffer[str_ren]=0;
+        for ( i = 0; i < real->len; i++)
+        {
+            write(real->socks[i],buffer,BUF_SZIE);
+        }     
+    }
+    delete buffer;
+    delete real;
+    return NULL;
 }
+//채팅방 함수 msg를 통해 데이터가 들어오고 2명에게 모두 읽어온 데이터를 보냄
+void* chatingroom(void* socks)
+{
+    chatingparts myparts=*(chatingparts*)socks;
+    chatingparts* real=new chatingparts;
+    epoll_event *epoll_events;
+    epoll_event event;
+    *real=myparts;
+    int str_ren,i;
+    int epfd,event_cnt;
+    string k;
+    char* buffer = new char[BUF_SZIE];
+    for ( i = 0; i < real->len; i++)
+    {
+        event.events=EPOLLIN;
+        event.data.fd=real->socks[i];
+        epoll_ctl(epfd,EPOLL_CTL_ADD,real->socks[i],&event);
+    }
+    
 
+    while (1)
+    {
+        for ( i = 0; i < event_cnt; i++)
+        {
+            str_ren=read(event[i])
+        }
+        
+
+    }
+    delete buffer;
+    delete real;
+    return NULL;
+} 
 
 void send_clnt(string msg,int sock)
 {   
@@ -199,11 +248,15 @@ void send_clnt(string msg,int sock)
 }
 
 
-void chatprotocol(int checkf,int clnt_sock,int* login,chatserver* my,char msg[BUF_SZIE],string* aka)
+void chatprotocol(int checkf,int clnt_sock,int* login,chatserver* my,char msg[BUF_SZIE],string* aka,int* status)
 {
     string copy=msg;
+    pthread_t t_id;
     cout<<copy<<endl;
     string k,z;
+    int uyt;
+    void* ga;
+    chatingparts sock;
     switch (checkf)
     {
     case 1:
@@ -241,16 +294,33 @@ void chatprotocol(int checkf,int clnt_sock,int* login,chatserver* my,char msg[BU
         break;
     case 4:
         /*1:1채팅요청함수*/
+        sock=oneread_oneread(copy);
+        uyt=plzone_one(login,sock);
+        cout<<uyt<<endl;
+        
+        if(uyt==1)
+        {
+           gosock(sock);
+           *status=1;
+           pthread_create(&t_id,NULL,chatingroom,(void*)&sock);
+           pthread_detach(t_id);
+        }
+            
         memset(msg,0,BUF_SZIE);
         break;
+    
     case 5:
-        /*채팅방요청함수*/
+
+        pthread_create(&t_id,NULL,okchatingroom,(void*)&clnt_sock);
+        pthread_detach(t_id);
         memset(msg,0,BUF_SZIE);
         break;
+    
     case 6:
-        /*랜덤채팅요청함수*/
+         
         memset(msg,0,BUF_SZIE);
         break;            
+    
     default:
         memset(msg,0,BUF_SZIE);
         break;
@@ -260,10 +330,17 @@ void chatprotocol(int checkf,int clnt_sock,int* login,chatserver* my,char msg[BU
 //앞에 숫자 때어버리기
 string splitchatmsg(string msg)
 {
-    stringstream iss(msg);
-    string buffer[2];
-    iss>>buffer[0]>>buffer[1];
-    return buffer[1];
+    string buffer=msg;
+    buffer=buffer.substr(buffer.find(" "));
+    return buffer;
+}
+
+void gosock(chatingparts my)
+{    
+    for (int i = 0; i < my.len; i++)
+    {
+        write(my.socks[i],(void*)&my,sizeof(chatingparts));
+    }   
 }
 
 void chat_msg(string clnts,char * msg, int len)
@@ -303,6 +380,8 @@ void chat_msg(string clnts,char * msg, int len)
 
 
 //현재 접속한 친구를 출력해주는 함수 
+
+
 string findonlinefriend(string msg)
 {
     stringstream iss(msg);
